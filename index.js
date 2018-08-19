19f19cacf34d83decfaba8e1b893f61643804f5d
cezarant <cezarantsouza@gmail.com>
2018-08-19 16:00:52 -0300

    inclusao das variaveis de ambiente

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var port = process.env.PORT || 3000;
 var app = require('express')();
 var mqtt    = require('mqtt');
-var mqtthost = 'm13.cloudmqtt.com';
 var client; 
 /***********************  Seção MQTT *****************************/
-var options = {
-	host: mqtthost,
-	port: 'xxx',
-	protocolId: 'xx',
-	secureProtocol: 'xxx',
-	protocolId: 'xx',
-	protocolVersion: 'x',
-	username: 'xx',
-  	password: 'x'
+var options = 
+{
+	host: process.env.mqtthost,
+	port: process.env.porta,
+	protocolId: process.env.protocolId,
+	secureProtocol: process.env.secureProtocol,	
+	protocolVersion: process.env.protocolVersion,
+	username: process.env.username,
+  	password: process.env.password
 };
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -21,37 +20,42 @@ function iniciaMQTT()
     client= mqtt.connect(options);
     client.on('connect', function ()
     {
-      client.subscribe(x);  		
+      client.subscribe(process.env.topicosubscribe);  		
     });
 
     client.on('message', function (topic, message)
     {  
        console.log("Mensagem recebida: "+ message.toString());       
        var json = JSON.parse(message.toString());
-       preparaEstoque(json.id,json.retira);	
+       // preparaEstoque(json.id,json.retira);	
        comunicaAoCliente("Componente retirado:" + json.nome);			       	       
     });
 }
 /******************* Seção REST **********************************/
+
 app.get('/iniciamqtt', function (request, response){
   iniciaMQTT();
   response.end('Inicia MQTT');
 });
 
 app.get('/retiraled', function (request, response){
-  client.publish('/xx','80');       
+  client.publish(process.env.topicosubscribe,'80');       
   response.end('Led_retirado');  
 });
 
 app.get('/devolveled', function (request, response){
-  client.publish('/xx','80');  
+  client.publish(process.env.topicosubscribe,'80');    
   response.end('Led_devolvido');
 });
 
-app.get('/retirabutton', function (request, response)
-{
-  client.publish('/xx','120');    
+app.get('/retirabutton', function (request, response){
+  client.publish(process.env.topicosubscribe,'120');    
   response.end('Button_retirado');
+});
+
+app.get('/devolvebutton', function (request, response){
+  client.publish(process.env.topicosubscribe,'120'); 
+  response.end('Button_devolvido');
 });
 /**************************** Seção HTTP **********************************/
 http.listen(port, function()
@@ -62,7 +66,7 @@ http.listen(port, function()
 app.get('/', function(req, res)
 {  
   res.sendFile(__dirname + '/index.html');
-});
+}); 
 
 io.on('connection', function(socket)
 {
@@ -76,7 +80,6 @@ function comunicaAoCliente(msg)
 {
    io.emit('chat message', msg);
 } 
-
 function mediador(etapa)
 {
     console.log(etapa); 
