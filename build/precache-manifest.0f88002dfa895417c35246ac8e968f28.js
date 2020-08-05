c966ddfd046cce903950d685bc8d1f8d15ebe17f
Cezar <cezar.sousa@sysmap.com.br>
2020-08-05 16:11:23 -0300

    Colocando arquivos react

---
+++ build/precache-manifest.0f88002dfa895417c35246ac8e968f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b38cf0f7e73568c9b7738ba3c25a597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a9803233b44ca471ec3",
+    "url": "/static/css/main.8abe9730.chunk.css"
+  },
+  {
+    "revision": "638d874098ae332cf5f9",
+    "url": "/static/js/2.908ef0e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.908ef0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9803233b44ca471ec3",
+    "url": "/static/js/main.47726175.chunk.js"
+  },
+  {
+    "revision": "bb5b99f98ed355d5cb58",
+    "url": "/static/js/runtime-main.e4368b2c.js"
+  },
+  {
+    "revision": "d1c00c4d33f55e30401d06f40df7d4f6",
+    "url": "/static/media/avatar.d1c00c4d.png"
+  }
+]);
